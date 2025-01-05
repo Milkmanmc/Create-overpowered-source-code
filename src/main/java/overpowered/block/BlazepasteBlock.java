3400dd642d1fd508f496c146855efbf9a85539ad
Milkman <132010718+Milkmanmc@users.noreply.github.com>
2025-01-05 16:57:15 +0100

    Add files via upload

--- src/main/java/overpowered/block/BlazepasteBlock.java
+++ src/main/java/overpowered/block/BlazepasteBlock.java
@@ -12,13 +12,25 @@ import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.LiquidBlock;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.core.Direction;
 import net.minecraft.core.BlockPos;
 
 public class BlazepasteBlock extends LiquidBlock {
 	public BlazepasteBlock() {
-		super(() -> CreateOverpoweredModFluids.BLAZE_PASTE.get(),
-				BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_YELLOW).strength(100f).noCollission().noLootTable().liquid().pushReaction(PushReaction.DESTROY).sound(SoundType.EMPTY).replaceable());
+		super(() -> CreateOverpoweredModFluids.BLAZE_PASTE.get(), BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_YELLOW).strength(100f).hasPostProcess((bs, br, bp) -> true).emissiveRendering((bs, br, bp) -> true).noCollission().noLootTable()
+				.liquid().pushReaction(PushReaction.DESTROY).sound(SoundType.EMPTY).replaceable());
+	}
+
+	@Override
+	public int getFireSpreadSpeed(BlockState state, BlockGetter world, BlockPos pos, Direction face) {
+		return 5;
+	}
+
+	@Override
+	public int getLightBlock(BlockState state, BlockGetter worldIn, BlockPos pos) {
+		return 3;
 	}
 
 	@Override
--- src/main/java/overpowered/init/CreateOverpoweredModTabs.java
+++ src/main/java/overpowered/init/CreateOverpoweredModTabs.java
@@ -18,25 +18,25 @@ public class CreateOverpoweredModTabs {
 	public static final DeferredRegister<CreativeModeTab> REGISTRY = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, CreateOverpoweredMod.MODID);
 	public static final RegistryObject<CreativeModeTab> CREATE_OVERPOWERED = REGISTRY.register("create_overpowered",
 			() -> CreativeModeTab.builder().title(Component.translatable("item_group.create_overpowered.create_overpowered")).icon(() -> new ItemStack(CreateOverpoweredModItems.ROUGH_DIAMOND.get())).displayItems((parameters, tabData) -> {
-				tabData.accept(CreateOverpoweredModItems.ROUGH_DIAMOND.get());
-				tabData.accept(CreateOverpoweredModItems.STALE_REDSTONE_POWDER.get());
-				tabData.accept(CreateOverpoweredModItems.NETHERITE_SCRAP_NUGGET.get());
-				tabData.accept(CreateOverpoweredModItems.BLAZE_PASTE_BUCKET.get());
-				tabData.accept(CreateOverpoweredModItems.THE_LAST_FIGHT.get());
-				tabData.accept(CreateOverpoweredModItems.HYPER_ENCHANTED_GOLDEN_APPLE.get());
+				tabData.accept(CreateOverpoweredModBlocks.MORAISNORE.get().asItem());
+				tabData.accept(CreateOverpoweredModItems.MORAISN_CRYSTAL.get());
 				tabData.accept(CreateOverpoweredModItems.STICKY_MORAISN_COATING.get());
+				tabData.accept(CreateOverpoweredModItems.MORAISN_COATED_NETHERTIE_UPGRADE_SMITHING_TEMPLATE.get());
 				tabData.accept(CreateOverpoweredModItems.MORAISN_COUTED_NETHERITE_ARMOR_HELMET.get());
 				tabData.accept(CreateOverpoweredModItems.MORAISN_COUTED_NETHERITE_ARMOR_CHESTPLATE.get());
 				tabData.accept(CreateOverpoweredModItems.MORAISN_COUTED_NETHERITE_ARMOR_LEGGINGS.get());
 				tabData.accept(CreateOverpoweredModItems.MORAISN_COUTED_NETHERITE_ARMOR_BOOTS.get());
-				tabData.accept(CreateOverpoweredModItems.MORAISN_COATED_NETHERTIE_UPGRADE_SMITHING_TEMPLATE.get());
-				tabData.accept(CreateOverpoweredModItems.MORAISN_COATED_NETHERITE_AXE.get());
 				tabData.accept(CreateOverpoweredModItems.MORAISN_COATED_NETHERITE_SWORD.get());
+				tabData.accept(CreateOverpoweredModItems.MORAISN_COATED_NETHERITE_AXE.get());
+				tabData.accept(CreateOverpoweredModItems.MORAISN_COATED_NETHERITE_PICKAXE.get());
 				tabData.accept(CreateOverpoweredModItems.MORAISN_COATED_NETHERITE_SHOVEL.get());
 				tabData.accept(CreateOverpoweredModItems.MORAISN_COATED_NETHERITE_HOE.get());
-				tabData.accept(CreateOverpoweredModItems.MORAISN_COATED_NETHERITE_PICKAXE.get());
-				tabData.accept(CreateOverpoweredModItems.MORAISN_CRYSTAL.get());
-				tabData.accept(CreateOverpoweredModBlocks.MORAISNORE.get().asItem());
+				tabData.accept(CreateOverpoweredModItems.ROUGH_DIAMOND.get());
+				tabData.accept(CreateOverpoweredModItems.STALE_REDSTONE_POWDER.get());
+				tabData.accept(CreateOverpoweredModItems.NETHERITE_SCRAP_NUGGET.get());
+				tabData.accept(CreateOverpoweredModItems.BLAZE_PASTE_BUCKET.get());
+				tabData.accept(CreateOverpoweredModItems.HYPER_ENCHANTED_GOLDEN_APPLE.get());
+				tabData.accept(CreateOverpoweredModItems.THE_LAST_FIGHT.get());
 			})
 
 					.build());
